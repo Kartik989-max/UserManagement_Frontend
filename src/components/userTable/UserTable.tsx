52dabf85c5222eb78e2ec909ae0afed51bbdb6d5
Kartik989-max <bhatnagarkartik6.com>
2025-05-08 03:18:07 +0530

    main

--- src/components/userTable/UserTable.tsx
+++ src/components/userTable/UserTable.tsx
@@ -11,6 +11,8 @@ const UserTable: React.FC = () => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
+        console.log(`${process.env.REACT_APP_API_BASE_URL}/user`);
+        
         const res = await axios.get<User[]>( `${process.env.REACT_APP_API_BASE_URL}/user`);
         setUser(res.data);
         setFilteredUser(res.data);
